88d932f7fc791c998dff916af2d781fa6fb13bac
Mustbe112 <winlwinmoe33@gmail.com>
2026-06-07 20:29:52 +0700

    url fix

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://ai-chat-bot-clinic.vercel.app/'  // your actual Vercel URL
+    'https://ai-chat-bot-clinic.vercel.app'  // your actual Vercel URL
   ]
 }))
 app.use(express.json())
